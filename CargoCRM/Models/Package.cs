417a7dbc399702117e36753e36f99c4283c60537
MrShahrom <shahromeo777@gmail.com>
2025-04-21 14:48:20 +0500

    Added models

---
+++ CargoCRM/Models/Package.cs
@@ -0,0 +1,13 @@
+﻿namespace CargoCRM.Models;
+
+public class Package
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Id_trackcode { get; set; }
+    public bool Is_sent { get; set; }
+    public DateTime Date_from { get; set; }
+    public DateTime Date_to { get; set; }
+    public DateTime Created_at { get; set; } = DateTime.Now;
+    public DateTime Updated_at { get; set; }
+}
---
+++ CargoCRM/Models/TrackCode.cs
@@ -0,0 +1,10 @@
+﻿namespace CargoCRM.Models;
+
+public class TrackCode
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Id_customer { get; set; }
+    public DateTime Created_at { get; set; } = DateTime.Now;
+    public DateTime Updated_at { get; set; }
+}
